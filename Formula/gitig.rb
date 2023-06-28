a54d2dfeccefc30e82a4b0dca722ec6d01a066e6
lou <gold24park@gmail.com>
2023-06-28 22:03:21 +0900

    v0.0.1

--- Formula/gitig.rb
+++ Formula/gitig.rb
@@ -1,21 +1,14 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Espanso < Formula
-  desc "Creates a gitignore for the configured project."
-  homepage "https://github.com/federico-terzi/espanso"
-  url "https://github.com/federico-terzi/espanso/releases/download/v0.7.3/espanso-mac.tar.gz"
-  sha256 "aa9c74f47e3a545657f45a15c97f455466b257a8e146172c74b425d06a3c9b9c"
-  version "0.7.3"
-
-  resource "modulo" do
-    url "https://github.com/federico-terzi/modulo/releases/download/v0.1.1/modulo-mac"
-    sha256 "8e27979d1ba37b5e1ea70dd1928da84e8f12b926b53a16e2b1388f536042cb06"
-  end
-
-  def install
-    bin.install "espanso"
-
-    resource("modulo").stage { bin.install "modulo-mac" => "modulo" }
-  end
+class Gitig < Formula
+    desc "Creates a gitignore for the configured project."
+    homepage "https://github.com/gold24park/gitig"
+    url "https://github.com/gold24park/gitignore/releases/download/v0.0.1/gitig.tar.gz"
+    version "0.0.1"
+    sha256 "2e72e743ee3d52773d982bb57314db8b320ae8a4404364e16742fcae9a7b8d28"
+  
+    def install
+      bin.install "gitig"
+    end
 end
