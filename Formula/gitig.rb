9cf7190364079aca4a6fd04cbcf0a41894dcf29f
lou <gold24park@gmail.com>
2023-07-01 09:31:09 +0900

    v0.0.2

--- Formula/gitig.rb
+++ Formula/gitig.rb
@@ -1,9 +1,9 @@
 class Gitig < Formula
     desc "Creates a gitignore for the configured project."
     homepage "https://github.com/gold24park/gitig"
-    url "https://github.com/gold24park/gitignore/releases/download/v0.0.1/gitig.tar.gz"
-    version "0.0.1"
-    sha256 "2e72e743ee3d52773d982bb57314db8b320ae8a4404364e16742fcae9a7b8d28"
+    url "https://github.com/gold24park/gitignore/releases/download/v0.0.2/gitig.tar.gz"
+    version "0.0.2"
+    sha256 "617019aa5b8e000e76dc20750318a938e8aa26914a7bcda1b4e0fc4b80d9cf83"
   
     def install
       bin.install "gitig"
